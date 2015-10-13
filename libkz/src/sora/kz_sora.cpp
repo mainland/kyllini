21e124299b7e899bc232cdeefb38f054fcb9063f
Geoffrey Mainland <mainland@drexel.edu>
2015-10-12 22:45:28 -0400

    Abort when attempting to use viterbi functions.

--- libkz/src/sora/kz_sora.cpp
+++ libkz/src/sora/kz_sora.cpp
@@ -317,14 +317,20 @@ void __kz_sora_fft(int n, const complex16_t *in, complex16_t *out)
 
 void __kz_viterbi_brick_init_fast(int32_t frame_length, int16_t code_rate, int16_t depth)
 {
+    fprintf(stderr, "viterbi_brick_init_fast not implemented\n");
+    abort();
 }
 
 int16_t __kz_viterbiSig11a_brick_decode_fast(int n, const int8_t svalue[48], const uint8_t *bitValue)
 {
+    fprintf(stderr, "viterbiSig11a_brick_decode_fast not implemented\n");
+    abort();
     return 0;
 }
 
 int16_t __kz_viterbi_brick_decode_fast(int n, const int8_t svalue[48], const uint8_t *bitValue)
 {
+    fprintf(stderr, "viterbiSig11a_brick_decode_fast not implemented\n");
+    abort();
     return 0;
 }
