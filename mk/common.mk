5f28fa8dc5567dab33b4397358f808d7b3731de6
Geoffrey Mainland <mainland@drexel.edu>
2016-05-16 18:16:39 -0400

    Don't dump intermediate stages by default.
    This should speed up validation a bit.

--- mk/common.mk
+++ mk/common.mk
@@ -124,7 +124,7 @@ $(BLINKDIFF) : $(TOP)/tools/BlinkDiff.hs
 TESTDIR = $(TOP)/testsuite
 
 KZC=$(TOP)/kzc
-KZCFLAGS+=-I$(TESTDIR)/lib --dlint --ddump-core --ddump-lift --ddump-fusion --ddump-simpl --dprint-uniques --fno-line-pragmas
+KZCFLAGS+=-I$(TESTDIR)/lib --dlint --dprint-uniques --fno-line-pragmas
 
 RUNTIME_SRC=\
 	$(TOP)/libkz/src/bits.c \
