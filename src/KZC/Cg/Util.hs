416a529b15ee009aaf43466ee973c8c1cbe9e2c8
Geoffrey Mainland <mainland@drexel.edu>
2016-08-03 16:44:48 -0400

    Add ToBlockItems type class.
    The ToBlockItems class is for types that can be converted to a list of C
    BlockItem's.

--- src/KZC/Cg/Util.hs
+++ src/KZC/Cg/Util.hs
@@ -6,15 +6,40 @@
 
 module KZC.Cg.Util (
     ToInitializer(..),
+    ToBlockItems(..),
     zencode
   ) where
 
 import Data.Char (ord)
+import Data.Foldable (toList)
+import Data.Sequence (Seq)
 import Numeric (showHex)
 import qualified Language.C.Quote as C
 
 class ToInitializer a where
     toInitializer :: a -> C.Initializer
+
+class ToBlockItems a where
+    toBlockItems :: a -> [C.BlockItem]
+
+    toBlockItemsList :: [a] -> [C.BlockItem]
+    toBlockItemsList = concatMap toBlockItems
+
+instance ToBlockItems a => ToBlockItems [a] where
+    toBlockItems = toBlockItemsList
+
+instance ToBlockItems a => ToBlockItems (Seq a) where
+    toBlockItems = toBlockItemsList . toList
+
+instance ToBlockItems C.Stm where
+    toBlockItems stm = [C.BlockStm stm]
+
+    toBlockItemsList = map C.BlockStm
+
+instance ToBlockItems C.InitGroup where
+    toBlockItems decl = [C.BlockDecl decl]
+
+    toBlockItemsList = map C.BlockDecl
 
 -- | Z-encode a string. This converts a string with special characters into a
 -- form that is guaranteed to be usable as an identifier by a C compiler or
