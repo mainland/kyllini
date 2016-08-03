7056ab09e4aff6d8d8c0199d18a995f6dc71678a
Geoffrey Mainland <mainland@drexel.edu>
2016-08-03 08:59:20 -0400

    Remove unnecessary class constraint.

--- src/KZC/Cg.hs
+++ src/KZC/Cg.hs
@@ -569,7 +569,7 @@ cgLocalDecl _flags LetViewLD{} _k =
 -- | Generate a 'CExp' representing a constant. The 'CExp' produced is
 -- guaranteed to be a legal C initializer, so it can be used in an array or
 -- struct initializer.
-cgConst :: forall l . IsLabel l => Const -> Cg l (CExp l)
+cgConst :: forall l . Const -> Cg l (CExp l)
 cgConst UnitC            = return CVoid
 cgConst (BoolC b)        = return $ CBool b
 cgConst (FixC I _ _ 0 x) = return $ CInt (fromIntegral x)
