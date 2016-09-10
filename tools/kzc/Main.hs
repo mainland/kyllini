2ade552390a398090af0b177236e8c5b3db44cf9
Geoffrey Mainland <mainland@drexel.edu>
2016-09-10 15:51:02 -0400

    Split library and compiler into separate builds.

---
+++ tools/kzc/Main.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE CPP #-}
+
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) 2015-2016 Drexel University
+-- License     :  BSD-style
+-- Maintainer  :  mainland@cdrexel.edu
+
+module Main (
+    main
+  ) where
+
+import KZC.Driver
+
+main :: IO ()
+main = defaultMain
