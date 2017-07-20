c6ca37c8ef3086eed426107a91f97bc54767566a
Geoffrey Mainland <mainland@drexel.edu>
2017-07-19 21:52:27 -0400

    Fix IO under MinGW.

--- libkz/src/io.cpp
+++ libkz/src/io.cpp
@@ -183,7 +183,8 @@ template<typename T> void init_input(const kz_params_t* params, kz_buf_t* buf)
             T       x;
             bool    success;
 
-            text = (char*) read_file(params->src, "r", &text_len);
+            // Always open file in binary mode. If we don't, we fail on MinGW.
+            text = (char*) read_file(params->src, "rb", &text_len);
             assert(text != NULL);
 
             buf->buf = malloc(size*sizeof(T));
@@ -431,7 +432,8 @@ void kz_init_input_bit(const kz_params_t* params, kz_buf_t* buf)
             bit_t   x;
             bool    success;
 
-            text = (char*) read_file(params->src, "r", &text_len);
+            // Always open file in binary mode. If we don't, we fail on MinGW.
+            text = (char*) read_file(params->src, "rb", &text_len);
             assert(text != NULL);
 
             buf->buf = malloc((size + BIT_ARRAY_ELEM_BITS - 1)/BIT_ARRAY_ELEM_BITS);
