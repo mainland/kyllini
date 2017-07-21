mainland/kyllini

commit fa5d48db2dfd7c46eb5ec8d8d2bf4ae0497cf75b

Simplify away redundant casts.

src/KZC/Optimize/Simplify.hs
@@ -1196,6 +1196,14 @@ simplE (UnopE op e s) = do
         rewrite
         return $ ConstE c' s
 
+    -- Avoid double cast
+    unop (Cast tau) e = do
+        tau' <- inferExp e
+        if tau' == tau
+          then do rewrite
+                  return e
+          else return $ UnopE op e s
+
     -- Remove duplicate bitcast operations.
     unop op@Bitcast{} (UnopE Bitcast{} e s) = do
         rewrite

commit d96e8fac4bc46eb47bed92fb6ff5b6f64a5c8c2d

Ensure we use simplified types in casts.
This name-shadowing bug was introduced in fa5d48db.

src/KZC/Optimize/Simplify.hs
@@ -1197,7 +1197,7 @@ simplE (UnopE op e s) = do
         return $ ConstE c' s
 
     -- Avoid double cast
-    unop (Cast tau) e = do
+    unop op@(Cast tau) e = do
         tau' <- inferExp e
         if tau' == tau
           then do rewrite