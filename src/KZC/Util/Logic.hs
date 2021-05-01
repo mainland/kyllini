0e4aa1bd35528b59a19e4f4ac73c613e4a7636a7
Geoffrey Mainland <mainland@drexel.edu>
2021-05-01 15:56:16 -0400

    Use Alternative rather than MonadPlus methods.
    This maintains compatibility with the most recent version of the logict
    package (0.7.1.0).

--- src/KZC/Util/Logic.hs
+++ src/KZC/Util/Logic.hs
@@ -2,7 +2,7 @@
 
 -- |
 -- Module      :  KZC.Util.Logic
--- Copyright   :  (c) 2016 Drexel University
+-- Copyright   :  (c) 2016-2021 Drexel University
 -- License     :  BSD-style
 -- Maintainer  :  mainland@drexel.edu
 
@@ -10,13 +10,12 @@ module KZC.Util.Logic (
     prune
   ) where
 
-import Control.Monad (mplus,
-                      mzero)
+import Control.Applicative ((<|>), empty)
 import Control.Monad.Logic (MonadLogic,
                             msplit)
 
 -- | Prune after @n@ attempts.
 prune :: MonadLogic m => Int -> m a -> m a
-prune 0  _ = mzero
-prune !n m = do (a, m') <- maybe mzero return =<< msplit m
-                return a `mplus` prune (n-1) m'
+prune 0  _ = empty
+prune !n m = do (a, m') <- maybe empty return =<< msplit m
+                return a <|> prune (n-1) m'
